5f259333a60addb949997083098579973009f8fb
John Papa <john@Johns-MBP.lan>
2017-04-24 09:38:54 -0400

    debugging

--- services/filters.js
+++ services/filters.js
@@ -3,7 +3,11 @@ module.exports = {
 };
 
 function getMatch(list, id) {
+  context.log(list);
+  context.log(id);
   const matches = list.filter(item => item.id === id);
+  context.log(matches);
   const match = matches.length ? match[0] : {};
+  context.log(match);
   return match;
 }
