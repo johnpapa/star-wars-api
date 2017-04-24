9eaffe3ed196e775afaac8a3659a47a1c60b01e9
John Papa <john@Johns-MBP.lan>
2017-04-24 09:47:18 -0400

    debug

--- services/filters.js
+++ services/filters.js
@@ -7,7 +7,7 @@ function getMatch(context, list, id) {
   context.log(id);
   const matches = list.filter(item => item.id === id);
   context.log(matches);
-  const match = matches.length ? match[0] : {};
+  const match = matches.length ? matches[0] : {};
   context.log(match);
   return match;
 }
