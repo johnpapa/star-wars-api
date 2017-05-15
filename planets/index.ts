5a578263b0525cb927c07a153a5f364f5c47901d
John Papa <john@Johns-MBP.lan>
2017-05-15 19:18:12 -0400

    typescript

--- planets/index.ts
+++ planets/index.ts
@@ -1,25 +1,30 @@
-module.exports = function (context, request) {
+export async function run(context, request) {
+  import { planetService } from '../services/planets';
+  import { filters } from '../services/filters';
+
   context.log('JavaScript HTTP trigger function processed a request.');
   context.log('Bindings', context.bindings);
   context.log('Request', request);
 
   const id = parseInt(context.bindingData.id);
 
-  let planetService = require('../services/planets');
-  let filters = require('../services/filters');
+  let body: string;
+  let status = 200;
   let data = planetService.getPlanets();
   let response = {};
 
   if (id) {
     const planet = filters.getMatch(data.results, id);
-    response = {
-      body: planet
-    };
+    body = planet;
   }
   else {
-    response = {
-      body: data
-    };
+    body = data;
   }
+
+  context.res = {
+    body: body,
+    status: status
+  };
+
   context.done(null, response);
 };
