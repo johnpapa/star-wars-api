7cf180f785f950533afabe836fd5a9586b36d276
John Papa <john@Johns-MBP.lan>
2017-05-15 19:54:21 -0400

    ping func

--- ping/index.ts
+++ ping/index.ts
@@ -1,26 +1,11 @@
-import { getPlanets } from '../services/planets';
-import { getMatch } from '../services/filters';
-
 export async function run(context, request) {
-
   context.log('JavaScript HTTP trigger function processed a request.');
   context.log('Bindings', context.bindings);
   context.log('Request', request);
 
-  const id = parseInt(context.bindingData.id);
-
-  let body: string | {};
+  let body: string | {} = 'hello there';
   let status = 200;
-  let data = getPlanets();
   let response: { body: string | {}, status: number};
-
-  if (id) {
-    const planet = getMatch(data.results, id);
-    body = planet;
-  }
-  else {
-    body = data;
-  }
 
   response = {
     body: body,
