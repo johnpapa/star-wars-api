131caf92aa7b6f36e4b893070eefa39c473bd7c5
John Papa <john@Johns-MBP.lan>
2017-05-15 19:58:11 -0400

    ping

--- ping/index.ts
+++ ping/index.ts
@@ -1,13 +1,8 @@
 export async function run(context, request) {
-  context.log('JavaScript HTTP trigger function processed a request.');
-  context.log('Bindings', context.bindings);
-  context.log('Request', request);
-
-  let body: string | {} = 'hello there';
+  let body = 'hello there';
   let status = 200;
-  let response: { body: string | {}, status: number};
 
-  response = {
+  let response = {
     body: body,
     status: status
   };
