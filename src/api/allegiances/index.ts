db424f3662266c068f45c86ce6fe59a0f06b010a
John Papa <john@Johns-MBP.lan>
2017-05-15 20:39:44 -0400

    moving to their own path

---
+++ src/api/allegiances/index.ts
@@ -0,0 +1,19 @@
+import { getAllegiances } from '../../services/people';
+
+export async function run(context, request) {
+  context.log('JavaScript HTTP trigger function processed a request.');
+  context.log('Bindings', context.bindings);
+  context.log('Request', request);
+
+  let body: string | {};
+  let status = 200;
+  let data = getAllegiances();
+  let response = {};
+
+  response = {
+    body,
+    status
+  }
+
+  return context.done(null, response);
+};
