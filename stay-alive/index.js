41bf077ecda3b10bfeee35dc0af113e70cdeb73c
John Papa <john@Johns-MBP.lan>
2017-05-19 10:16:08 -0400

    added timer function

---
+++ stay-alive/index.js
@@ -0,0 +1,11 @@
+module.exports = function (context, stayAliveTimer) {
+    var timeStamp = new Date().toISOString();
+
+    if(stayAliveTimer.isPastDue)
+    {
+        context.log('Timer is past due!');
+    }
+    context.log('JavaScript timer trigger function ran!', timeStamp);
+
+    context.done();
+};
